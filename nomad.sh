83c0842777bbedb97668d34274ac1224a4f4652b
Max Catman <xorgnak@gmail.com>
2024-07-11 14:31:00 -0600

    Update nomad.sh

--- nomad.sh
+++ nomad.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+git clone --depth=1 https://github.com/xorgnak/nomadian
+cd nomadian
+source nomadian.sh
+cd ..
+
 git clone --depth=1 https://github.com/propedicab/bin
 cd bin
 source nomad.sh
@@ -10,9 +16,6 @@ cd dev
 source nomad.sh
 cd ..
 
-git clone --depth=1 https://github.com/xorgnak/nomadian
-cd nomadian
-source nomadian.sh
-cd ..
+sudo gem install iww cardtrick fortunecookie llamafile maiden meiou pvector remind
 
 echo "[NOMAD] Done!"

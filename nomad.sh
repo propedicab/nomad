1f966b51a7903b6b370bcc2586139299fb5ef60d
Max Catman <xorgnak@gmail.com>
2024-07-11 14:44:12 -0600

    Update nomad.sh

--- nomad.sh
+++ nomad.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-
+# install debian overlay
 git clone --depth=1 https://github.com/xorgnak/nomadian
 cd nomadian
 source nomadian.sh
 cd ..
 
+# install utilities
 git clone --depth=1 https://github.com/propedicab/bin
 cd bin
 source nomad.sh
 cd ..
-  
+
+# download current device firmware
 git clone --depth=1 https://github.com/propedicab/dev
 cd dev
 source nomad.sh
 cd ..
-
-sudo gem install iww cardtrick fortunecookie llamafile maiden meiou pvector remind
 
 echo "[NOMAD] Done!"
